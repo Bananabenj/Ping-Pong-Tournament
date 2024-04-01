53a3d98d9d268df303a7dd441a98384a25a309ee
Bananabenj <benj.thibout@gmail.com>
2024-04-01 22:30:33 +0200

    1st Commit : all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TournoiComponent} from "./tournoi/tournoi.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TournoiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/tournoi/tournoi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournoi',
+  standalone: true,
+  imports: [],
+  templateUrl: './tournoi.component.html',
+  styleUrl: './tournoi.component.css'
+})
+export class TournoiComponent {
+
+}
